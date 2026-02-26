2fa656d85a013a9700fcd5bf39a388a5a3dcb555
Pragatireddy <reddypragati43@gmail.com>
2026-02-26 15:51:38 +0530

    Added

---
+++ patterns.py
@@ -0,0 +1,24 @@
+"""def star_pattern(n):
+    for i in range (1,n+1):
+        for j in range(n): 
+            print("*",end=" ")
+        print()
+def main():
+    n=int(input("enter a number :"))
+    star_pattern(n)
+main()
+"""
+
+def star_pattern(n):
+    for i in range (1,n+1):
+        for j in range(1,n+1):
+            if j>=i:
+                print(" ",end=" ")
+            else:
+                print("*", end=" ")
+        print()
+def main():
+    n=int(input("enter a number :"))
+    star_pattern(n)
+main()
+
